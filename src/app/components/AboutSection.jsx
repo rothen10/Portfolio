8b81699b665c4382c74d6a2f20b27c7f7e9420ca
rothen10 <rothensohmai@gmail.com>
2024-02-29 20:30:59 +0100

    portfolio1

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,100 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>MySQL</li>
+        <li>Next.js</li>
+        <li>JavaScript</li>
+        <li>React.js</li>
+        <li>Java</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Codeur Junior</li>
+        <li>Institut Universitaire de Technologie</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Java foundation</li>
+       
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" alt='about image' width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">Sur moi</h2>
+          <p className="text-base lg:text-lg">
+          Je suis un développeur Web full stack passionné par la création
+            Applications Web interactives et réactives. Je ai l'expérience
+            travailler avec JavaScript, React, Redux, Node.js, Express, PostgreSQL,
+            Sequelize, HTML, CSS et Git. J'apprends vite et je suis toujours
+            Je cherche à élargir mes connaissances et mes compétences. Je suis un joueur d'équipe et
+            Je suis ravi de travailler avec d'autres pour créer des applications étonnantes.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              compétences{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span>Rothen Soh</span>
+        <p className="text-slate-600">Tous droits reservés.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
